d4ccb394fec4c2b7b4f01bb7d44022281bef430f
pikachu49 <julio31529@gmail.com>
2016-09-30 14:16:26 -0500

    eliminar comflictos injector.dart

--- lib/config/injector.dart
+++ lib/config/injector.dart
@@ -3,30 +3,28 @@ import 'package:di/di.dart';
 import 'db_connection.dart';
 import '../repository/user_repository.dart';
 import '../repository/persona_repository.dart';
-<<<<<<< HEAD
 import '../repository/producto_repository.dart';
 
 import '../service/user_service.dart';
 import '../service/persona_service.dart';
 import '../service/producto_service.dart';
-=======
 import '../repository/usuario_repository.dart';
 
 
 import '../service/user_service.dart';
 import '../service/persona_service.dart';
 import '../service/usuario_service.dart';
->>>>>>> master
+
 
 
 import '../rest/user_rest.dart';
 import '../rest/hello_rest.dart';
 import '../rest/persona_rest.dart';
-<<<<<<< HEAD
+
 import '../rest/producto_rest.dart';
-=======
+
 import '../rest/usuario_rest.dart';
->>>>>>> master
+
 
 var injector = new ModuleInjector([new Module()
 
@@ -35,28 +33,21 @@ var injector = new ModuleInjector([new Module()
   // repositories
   ..bind(UserRepository)
   ..bind(PersonaRepository)
-<<<<<<< HEAD
+
   ..bind(ProductoRepository)
-=======
+
   ..bind(UsuarioRepository)
->>>>>>> master
 
   // services
   ..bind(UserService)
   ..bind(PersonaService)
-<<<<<<< HEAD
   ..bind(ProductoService)
-=======
   ..bind(UsuarioService)
->>>>>>> master
 
   // REST controllers
   ..bind(HelloRest)
   ..bind(UserRest)
   ..bind(PersonaRest)
-<<<<<<< HEAD
   ..bind(ProductoRest)
-=======
   ..bind(UsuarioRest)
->>>>>>> master
 ]);
