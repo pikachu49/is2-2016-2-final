2b37a1d2e11d9cc2de9cabc5b69a3968d5ced527
pikachu49 <julio31529@gmail.com>
2016-09-30 05:38:29 -0500

    implementacion clase persona

---
+++ SQL/persona.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Persona(
+	id INTEGER,
+	nombre VARCHAR(255),
+	email  VARCHAR(255),
+	telefono  VARCHAR(255),
+	direccion  VARCHAR(255)
+)
+
+
+
+INSERT INTO Persona(id,nombre,email,telefono,direccion)
+VALUES(1,'jesus','jesus.vera','98765','ucsp')
+
+
+INSERT INTO Persona(id,nombre,email,telefono,direccion)
+VALUES(1,'daniel','daniel.ramos','12345','ucsm')
