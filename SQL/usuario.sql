8ab7d204012d3742fa4233d117b613517dd32608
shadowM <jerikerl10@gmail.com>
2016-09-30 13:03:34 -0500

    agregando usuario.sql  SQL

---
+++ SQL/usuario.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE usuario
+(
+  id integer,
+  nick varchar(255),
+  passwd varchar(255),
+  tipo varchar(255)
+);
+
+INSERT INTO usuario(id,nick,passwd,tipo) VALUES(1,'chucho123','droidex','A');
+INSERT INTO usuario(id,nick,passwd,tipo) VALUES(2,'andre','chucho123','B');
