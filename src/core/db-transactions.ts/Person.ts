091c5346e455f8da3350c0c08dd7084a51373f80
jesusveca <jerikerl10@gmail.com>
2016-11-03 22:04:11 -0500

    transaction person updated

--- src/core/db-transactions.ts/Person.ts
+++ src/core/db-transactions.ts/Person.ts
@@ -1,9 +1,22 @@
 import {Person as PersonModel } from '../db-models/Person.ts';
+import {MongoModel} from '../classes/MongoModel.ts';
+import * as q from 'q';
+import config from '../../settings/index.ts';
 
 export function registerPerson (personData: PersonModel) {
-    
+    var deferred = q.defer();
+    var model = new MongoModel(config.dbConfig.models.person.name);
+    model.insert(personData).then(function (personInstance) {
+        deferred.resolve(personInstance);
+    }).catch(deferred.reject);
+    return deferred.promise;    
 }
 
-export function getPerson (personId: PersonModel) {
-    
+export function getPersonById (personId: PersonModel) {
+    var deferred = q.defer();
+    var model = new MongoModel(config.dbConfig.models.person.name);
+    model.findById(personId).then(function (personInstance) {
+        deferred.resolve(personInstance);
+    }).catch(deferred.reject);
+    return deferred.promise;
 }
