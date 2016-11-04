5e65905102eaae4754590297779675f4b8d670a0
Diego Silva <dsilva2401@gmail.com>
2016-11-04 01:18:41 -0500

    get provider handler updated

--- src/server/routers/handlers/provider/getProvider/handler.ts
+++ src/server/routers/handlers/provider/getProvider/handler.ts
@@ -1,4 +1,16 @@
 
 export function handler (req, res, next) {
-    res.end(':D');
+    
+    var providerId = req.params.providerId;
+    // Aqui se hace la consulta a la base de datos con el id
+    // estoy suponiendo que ya nos devolvio la informacion
+    var demoProvider = {
+        id: providerId,
+        name: 'Proveedor de prueba'
+    }
+
+    // ahora que ya tenemos la informacion se la enviamos al cliente
+    res.status(200)
+    res.json(demoProvider);
+    res.end();
 }
