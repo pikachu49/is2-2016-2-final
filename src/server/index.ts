2c645cff12a3aa5ea65197355b4f8fb34fde63a6
Diego Silva <dsilva2401@gmail.com>
2016-11-04 03:23:28 -0500

    server middlewares added

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,9 +3,17 @@ import {connectDatabase} from '../core/services/connectDatabase.ts';
 import {handlersRouter} from './routers/index.ts'; 
 import config from '../settings/index.ts';
 import * as path from 'path';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
 
 // Define Server
 var server  = new ExpressServer();
+
+// Setup middleware
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use(cookieParser());
 
 // Setup Server
 server.setupStatics('/statics', path.join(__dirname, 'statics'));
