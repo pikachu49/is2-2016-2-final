5bf2648792a8ebbbeb54c362ddb80a8f093e93c8
jesusveca <jerikerl10@gmail.com>
2016-11-03 22:40:31 -0500

    transaction session updated

--- src/core/db-transactions.ts/Session.ts
+++ src/core/db-transactions.ts/Session.ts
@@ -1,2 +1,22 @@
 import {Session as SessionModel} from '../db-models/Session.ts';
+import {MongoModel} from '../classes/MongoModel.ts';
+import * as q from 'q';
+import config from '../../settings/index.ts';
 
+export function registerSession (sessionData: SessionModel) {
+    var deferred = q.defer();
+    var model = new MongoModel(config.dbConfig.models.session.key);
+    model.insert(sessionData).then(function (sessionInstance) {
+        deferred.resolve(sessionInstance);
+    }).catch(deferred.reject);
+    return deferred.promise;
+}
+
+export function getSessionById (sessionId: string) {
+    var deferred = q.defer();
+    var model = new MongoModel(config.dbConfig.models.session.name);
+    model.findById(sessionId).then(function (sessionInstance) {
+        deferred.resolve(sessionInstance);
+    }).catch(deferred.reject);
+    return deferred.promise;
+}
