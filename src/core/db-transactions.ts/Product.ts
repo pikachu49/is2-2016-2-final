fd7b08a3433f77ac82b95768eeca60852ea9ac48
jesusveca <jerikerl10@gmail.com>
2016-11-03 21:59:17 -0500

    transaction product.ts updated

--- src/core/db-transactions.ts/Product.ts
+++ src/core/db-transactions.ts/Product.ts
@@ -1,9 +1,22 @@
 import {Product as ProductModel} from '../db-models/Product.ts';
+import {MongoModel} from '../classes/MongoModel.ts';
+import * as q from 'q';
+import config from '../../settings/index.ts';
 
 export function registerProduct (productData: ProductModel) {
-
+    var deferred = q.defer();
+    var model = new MongoModel(config.dbConfig.models.product.name);
+    model.insert(productData).then(function (productInstance) {
+        deferred.resolve(productInstance);
+    }).catch(deferred.reject);
+    return deferred.promise;
 }
 
-export function getProduct (productId: ProductModel) {
-    
+export function getProductById (productId: ProductModel) {
+    var deferred = q.defer();
+    var model = new MongoModel(config.dbConfig.models.product.name);
+    model.findById(productId).then(function (productInstance) {
+        deferred.resolve(productInstance);
+    }).catch(deferred.reject);
+    return deferred.promise;
 }
