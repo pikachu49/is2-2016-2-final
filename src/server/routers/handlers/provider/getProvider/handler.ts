2b44c5b7da3a1d97509aa4953382da462e2e9d60
Diego Silva <dsilva2401@gmail.com>
2016-11-05 17:22:54 -0500

    some bugs fixed

--- src/server/routers/handlers/provider/getProvider/handler.ts
+++ src/server/routers/handlers/provider/getProvider/handler.ts
@@ -4,7 +4,7 @@ import {getProviderById} from '../../../../../core/db-transactions/Provider.ts';
 export function handler (req, res, next) {
     
     var requestParams: RequestParams = req.body;
-    getProviderById(requestParams).then(function (providerInstance) {
+    getProviderById(req.params.providerId).then(function (providerInstance) {
         res.status(200);
         res.json(providerInstance);
         res.end();
