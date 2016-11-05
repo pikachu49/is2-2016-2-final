59c1b4a2939011d900eb3e37b372ac4de2e8e958
jesusveca <jerikerl10@gmail.com>
2016-11-05 01:42:49 -0500

    add transaction getProviders and Provider Handler

--- src/server/routers/handlers/provider/getProviders/handler.ts
+++ src/server/routers/handlers/provider/getProviders/handler.ts
@@ -1,4 +1,16 @@
 
+import {RequestParams} from './interface.ts';
+import {getProviders} from '../../../../../core/db-transactions/Provider.ts';
+
 export function handler (req, res, next) {
-    res.end(':D');
+    var requestParams: RequestParams = req.body;
+    getProviders().then(function (providerInstances) {
+        res.status(200);
+        res.json(providerInstances);
+        res.end();
+    }).catch(function (err) {
+        res.status(500);
+        res.json(err);
+        res.end();
+    });
 }
