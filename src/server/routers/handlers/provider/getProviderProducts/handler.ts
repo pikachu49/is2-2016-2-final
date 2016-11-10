8ef0206b2e57bc364996d24022108fe1422cb5f7
jesusveca <jerikerl10@gmail.com>
2016-11-10 00:23:47 -0500

    getProviderProducts

--- src/server/routers/handlers/provider/getProviderProducts/handler.ts
+++ src/server/routers/handlers/provider/getProviderProducts/handler.ts
@@ -1,3 +1,18 @@
+// export function handler (req, res, next) {
+//     res.end(':D');
+// }
+
+import {RequestParams} from './interface.ts';
+import {getProviderByProductId} from '../../../../../core/db-transactions/product.ts';
+
 export function handler (req, res, next) {
-    res.end(':D');
+    getProviderByProductId(req.params.providerId).then(function (productsInstances) {
+        res.status(200);
+        res.json(productsInstances);
+        res.end();
+    }).catch(function (err) {
+        res.status(500);
+        res.json(err);
+        res.end();
+    });
 }
