00a07b3015df6158f2a2cfb2bdf755e3359df7ea
Diego Silva <dsilva2401@gmail.com>
2016-11-23 10:20:28 -0500

    resources added

---
+++ src/server/statics/webapps/dashboard/src/app/services/Resources.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import * as q from 'q';
+import {Response, Http} from '@angular/http';
+
+@Injectable()
+export class Resources {
+
+    // Attributes
+
+    // Methods
+
+}
