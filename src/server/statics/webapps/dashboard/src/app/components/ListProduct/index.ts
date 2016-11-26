f586e6549bb74266ebff42fc827c6ef110a70e38
José Pablo Alpaca Rivera <jose.p.alpaca@gmail.com>
2016-11-26 16:21:12 -0500

    #80 List Product

--- src/server/statics/webapps/dashboard/src/app/components/ListProduct/index.ts
+++ src/server/statics/webapps/dashboard/src/app/components/ListProduct/index.ts
@@ -1,16 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../../../../../../../../core/db-models/Product.ts';
+import {Provider} from '../../../../../../../../core/db-models/Provider.ts';
+import {Resources} from '../../services/Resources.ts';
 
 @Component({
 	styles: [require('./style.styl').toString()],
 	template: require('./template.jade')(),
 })
-export class ListProductComponent {
+export class ListProductComponent implements OnInit {
 	
 	// Attributes
+		product: Product;
+		resources: Resources;
+		providers: Provider[];
 
 	// Methods
-		constructor () {
-			
+		constructor (resources: Resources) {
+			this.providers = [];
+			this.resources = resources;
+			this.product = {
+				name: '',
+				providerId: ''
+			}
+		}
+
+		ngOnInit () {
+			this.resources.getProviders({
+				urlParams: {},
+				data: {} 
+			}).subscribe((resp) => {
+				this.providers = resp;
+				console.log(this.providers);
+			})
+		}
+
+		getProducts(){
+			this.resources.getProviderProducts({
+				urlParams: { providerId: '' },
+				data: {}
+			}).subscribe((resp) => {
+				console.log(resp);
+			})
 		}
 
 }
