08222ca00634e962d10bca00e9dd6a3d7d695ebd
AndreLui1996 <andrechelsea3@gmail.com>
2016-11-26 16:02:36 -0500

    fomulario basico

--- src/server/statics/webapps/dashboard/src/app/components/RegisterProduct/index.ts
+++ src/server/statics/webapps/dashboard/src/app/components/RegisterProduct/index.ts
@@ -1,16 +1,32 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../../../../../../../../core/db-models/Product.ts';
+import {Resources} from '../../services/Resources.ts';
 @Component({
 	styles: [require('./style.styl').toString()],
 	template: require('./template.jade')(),
 })
-export class RegisterProductComponent {
+export class RegisterProductComponent implements OnInit {
 	
 	// Attributes
-
+		product: Product;
+		resources: Resources
 	// Methods
-		constructor () {
-			
+		constructor (resources: Resources) {
+			this.resources = resources;
+			this.product = {
+				name: ''
+			}
 		}
+		ngOnInit () {}
+		submitRegister () {
+			this.resources.RegisterProduct({
+				urlParams: {},
+				data: {
+					name: this.product.name 
+				} 
+			}).subscribe((resp) => {
+				console.log(resp);
+			})
+		}	
 
 }
