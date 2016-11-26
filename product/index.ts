25efb2e2991feef934f26f56fe3688a293c0b238
gretelcanasa <gretel.canasa@ucsp.edu.pe>
2016-11-26 16:01:52 -0500

    show detail product lot

---
+++ product/index.ts
@@ -0,0 +1,4 @@
+
+export {handler as getProduct} from './getProduct/handler.ts';
+export {handler as getProductLots} from './getProductLots/handler.ts';
+export {handler as registerProductLot} from './registerProductLot/handler.ts';
